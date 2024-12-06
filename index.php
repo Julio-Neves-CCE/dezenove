ba90048952062df68adf94e0abba75f2c7d7e167
julio-verne-1602 <165070641+julio-verne-1602@users.noreply.github.com>
2024-12-06 06:56:48 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="shortcut icon" href="assets/img/icons8-estrela-48.png" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <img src="assets/img/brasao.png" alt="">
+        <ul>
+            <li><a href="">Início</a></li>
+            <li><a href="">Estrutura</a></li>
+            <li><a href="">Quem somos?</a></li>
+            <li><a href="">Contato</a></li>
+        </ul>
+    </header>
+    <main>
+        <section>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ipsum cum animi eius laudantium alias impedit sint omnis aliquam voluptatibus! Ratione, magni rem? Repudiandae labore natus perferendis aliquid totam fuga!
+            <img src="" alt="">
+        </section>
+    </main>
+</body>
+</html>
